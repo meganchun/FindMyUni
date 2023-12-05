19b7ac912c51947d695c48df9b911c8970c4b548
Megan Chun <107379096+meganchun@users.noreply.github.com>
2023-12-04 21:57:54 -0500

    test

--- UniversityApp/src/model/User.java
+++ UniversityApp/src/model/User.java
@@ -15,6 +15,8 @@ public class User {
 	private List<Program> savedPrograms;
 	private List<String[]> marks;
 	
+	//sd
+	
 	//Constructor
 	public User(byte[] encryptedUsernameByte, byte[] encryptedPasswordByte) {
 		super();
